4325a46028cc2a6a03afbcfa51a0447cb42e15df
jalondono <juanlondono151776@hotmail.com>
2021-01-10 15:00:17 -0500

    [upload] using input in main.txt file

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ print_digits = __import__('str2digital').transform_numbers
 
 
 if __name__ == '__main__':
-    size = 4
-    expression = '0123456789'
-    print_digits(size, expression)
-    print()
+    while(True):
+        expression = input()
+        size = 3
+        print(expression)
+        print_digits(size, expression)
+        print()
