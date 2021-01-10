9c2fcfd5a85abab2658f8bdecec3482e32ca8458
jalondono <juanlondono151776@hotmail.com>
2021-01-10 15:03:16 -0500

    [upload] using input in main.txt file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print_digits = __import__('str2digital').transform_numbers
 
 
 if __name__ == '__main__':
-    while(True):
+    while True:
         expression = input()
         size = 3
         print(expression)
