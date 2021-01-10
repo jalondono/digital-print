d82ffdf49ad19d019067abf91f0b8bb3d719d5d9
jalondono <juanlondono151776@hotmail.com>
2021-01-10 15:52:30 -0500

    [upload] parse_input.py Contains a utils functions for validation and extraction of args from a text input [update] main.py Added call to validation functions and reading of inputs

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 print_digits = __import__('str2digital').transform_numbers
+verify_input = __import__('parse_input').verify_input
+str2list = __import__('parse_input').str2list
 
 
 if __name__ == '__main__':
     while True:
-        expression = input()
-        size = 3
-        print(expression)
+        print('Type an input and press ENTER')
+        text_input = input()
+        validated_input = verify_input(text_input)
+        arg_lists = str2list(validated_input)
+        size = int(arg_lists[0])
+        expression = arg_lists[1]
         print_digits(size, expression)
-        print()
+        if len(arg_lists) > 2:
+            exit()
+
